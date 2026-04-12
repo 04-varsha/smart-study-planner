824cf2ea47cd1fcae190a917b598c701ab6d4707
Prajwal U <prajwalumeshbsc24@rvu.edu.in>
2026-04-12 16:02:56 +0530

    changed loop 3 to 4

--- project.py
+++ project.py
@@ -5,11 +5,11 @@ for i in range(4):
     sub = input("Enter subject name: ")
     diff = int(input("Enter difficulty (0-10): "))
 
-    if 0 <= diff <= 4:
+    if 0 <= diff <= 3:
         hours = 2
-    elif 5 <= diff <= 8:
+    elif 4 <= diff <= 7:
         hours = 4
-    elif 9 <= diff <= 10:
+    elif 8 <= diff <= 10:
         hours = 6
     else:
         hours = 2
